6194fc0111609c5b22cf2eeb3ce800204f3e9512
Sa102515 <121575502+Sa102515@users.noreply.github.com>
2024-05-15 00:34:45 +0530

    Initial commit

---
+++ BackEnd/Server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const routes = require("./routes/ToDoRoute")
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json())
+app.use(cors())
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.log("error"));
+
+app.use(routes)
+
+app.listen(PORT, function () {
+  console.log(`listening on ${PORT}`);
+});
