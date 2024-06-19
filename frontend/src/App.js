670f8bff220f10b71ebd93b590b8a8bf1bb4ad55
Sa102515 <121575502+Sa102515@users.noreply.github.com>
2024-06-19 15:32:06 +0530

    connect Docker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,21 +20,21 @@ const updateMode = (_id , text) => {
 }
   return (
     <div className="App">
-      <div class="max-w-md mx-auto shadow-lg rounded-lg overflow-hidden mt-16">
-        <div class="px-4 py-2">
-          <h1 class="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
+      <div className="max-w-md mx-auto shadow-lg rounded-lg overflow-hidden mt-16">
+        <div className="px-4 py-2">
+          <h1 className="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
         </div>
 
-        <div class="flex items-center border-b-2 p-5 border-teal-500 py-2">
+        <div className="flex items-center border-b-2 p-5 border-teal-500 py-2">
           <input
-            class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
             placeholder="Add a task"
             value={text}
             onChange={(e)=> setText(e.target.value)}
           />
           <button
-            class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
             type="button"
             onClick={isUpdating ? 
             ()=>updateToDo(toDoId,text,setText,setToDo,setIsUpdating) :
@@ -44,7 +44,7 @@ const updateMode = (_id , text) => {
           </button>
         </div>
 
-        <div className="flex flex-col gap-3 m-2">
+        <div className="flex flex-col gap-3 my-2 xs:m-2">
           {toDo.map((item) => <ToDo 
           key={item._id} 
           text={item.text}
--- frontend/src/utils/HandleApi.js
+++ frontend/src/utils/HandleApi.js
@@ -4,42 +4,40 @@ const baseUrl = "http://localhost:5000"
 
 const getAllToDo = (setToDo) => {
     axios
-    .get(baseUrl)
-    .then(({data}) => {
-        console.log('data ---> ', data);
-        setToDo(data)
-    })
+        .get(baseUrl)
+        .then(({ data }) => {
+            setToDo(data)
+        })
 }
 
-const addTodo = (text , setText , setToDo) => {
+const addTodo = (text, setText, setToDo) => {
     axios
-    .post(`${baseUrl}/save` , {text})
-    .then((data) => {
-        console.log(data);
-        setText("")
-        getAllToDo(setToDo)
-    })
-    .catch((err)=>console.log("error"))
+        .post(`${baseUrl}/save`, { text })
+        .then((data) => {
+            setText("")
+            getAllToDo(setToDo)
+        })
+        .catch((err) => console.log("error"))
 }
 
-const updateToDo = (toDoId , text , setText ,  setToDo , setIsUpdating) => {
+const updateToDo = (toDoId, text, setText, setToDo, setIsUpdating) => {
     axios
-    .post(`${baseUrl}/update` , {_id:toDoId , text})
-    .then((data) => {
-        setText("")
-        setIsUpdating(false)
-        getAllToDo(setToDo)
-    })
-    .catch((err)=>console.log("error"))
+        .post(`${baseUrl}/update`, { _id: toDoId, text })
+        .then((data) => {
+            setText("")
+            setIsUpdating(false)
+            getAllToDo(setToDo)
+        })
+        .catch((err) => console.log("error"))
 }
 
-const deleteToDo = (_id ,setToDo ) => {
+const deleteToDo = (_id, setToDo) => {
     axios
-    .post(`${baseUrl}/delete` , {_id})
-    .then((data) => {
-        getAllToDo(setToDo)
-    })
-    .catch((err)=>console.log("error"))
+        .post(`${baseUrl}/delete`, { _id })
+        .then((data) => {
+            getAllToDo(setToDo)
+        })
+        .catch((err) => console.log("error"))
 }
 
-export {getAllToDo , addTodo , updateToDo ,deleteToDo}
+export { getAllToDo, addTodo, updateToDo, deleteToDo }
